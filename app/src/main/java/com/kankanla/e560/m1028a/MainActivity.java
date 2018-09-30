d3825b064d6a3fbb4c22e6a26306dd20a91d7c9a
E560 <koyi_s@hotmail.com>
2018-09-30 23:03:21 +0900

    23

--- app/src/main/java/com/kankanla/e560/m1028a/MainActivity.java
+++ app/src/main/java/com/kankanla/e560/m1028a/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
